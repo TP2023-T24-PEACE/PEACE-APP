cca89e0b9e94511e7ec22eab1d491255e6997177
Matej Pakán <mattpakan@gmail.com>
2024-01-27 02:54:32 +0100

    Functional navigation with 3 screens

--- app/src/main/java/com/t24/peaceapp/MainActivity.kt
+++ app/src/main/java/com/t24/peaceapp/MainActivity.kt
@@ -3,13 +3,22 @@ package com.t24.peaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.t24.peaceapp.destinations.HomeScreenDestination
+import com.t24.peaceapp.destinations.LoginScreenDestination
 import com.t24.peaceapp.ui.theme.PEACEAPPTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("PEACE APP")
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
     }
 }
 
+@Destination(start = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SplashScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Splash Screen", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(LoginScreenDestination)
+        }) {
+            Text(text = "Go to Login")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Destination
 @Composable
-fun GreetingPreview() {
-    PEACEAPPTheme {
-        Greeting("Android")
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login Screen", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(HomeScreenDestination)
+        }) {
+            Text(text = "Go to Home")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen", textAlign = TextAlign.Center)
     }
 }
