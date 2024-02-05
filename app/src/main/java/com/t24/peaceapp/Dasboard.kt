cc2ef6454e9839d466bf3543551f86ca012b673b
Adam Jankanič <xjankanic@stuba.sk>
2024-02-05 22:13:22 +0100

    changed Icon for Image in Dashboard.kt

--- app/src/main/java/com/t24/peaceapp/Dasboard.kt
+++ app/src/main/java/com/t24/peaceapp/Dasboard.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
