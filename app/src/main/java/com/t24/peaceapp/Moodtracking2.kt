4b59cf01a30b3430472f8fc4cc487a59b8eb5d0c
Adam Jankanič <xjankanic@stuba.sk>
2024-02-15 17:05:07 +0100

    unnecessary comments removed, Dashboard and Moodtracking1-3 design done

--- app/src/main/java/com/t24/peaceapp/Moodtracking2.kt
+++ app/src/main/java/com/t24/peaceapp/Moodtracking2.kt
@@ -6,10 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
